617d3297abe8b1b8dd3de3d1dd69c3961e6f343f
Melody Olvera <molvera@codeaurora.org>
2021-03-29 10:03:05 -0700

    QcomModulePkg: Update DDR UEFI headers and how HBB is found
    Update DDRDetails and EFIDDRGetConfig and the logic for finding
    the highest bank bit and adding it to the device tree. Also, remove
    now obsolete functions.
    
    Change-Id: I0c53f5d121f2d5a33993921a7ee56e148b697816

--- QcomModulePkg/Include/Library/UpdateDeviceTree.h
+++ QcomModulePkg/Include/Library/UpdateDeviceTree.h
@@ -1,4 +1,5 @@
-/* Copyright (c) 2015-2017, 2020, The Linux Foundation. All rights reserved.
+/* Copyright (c) 2015-2017, 2020-2021, The Linux Foundation.
+ * All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are
@@ -40,6 +41,8 @@
 #include <Protocol/EFILimits.h>
 #include <Protocol/EFIRamPartition.h>
 #include <Uefi.h>
+
+#define DDR_MAX_RANKS 2
 
 #define DTB_MAX_SUBNODE 128
 
